d4554ad90cde1c6cef954f9e8c4dc04f4147c0fc
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-11-01 20:11:35 +0000

    Implementando clase Receta y metodo comidayprop

--- lib/prct6/Dieta.rb
+++ lib/prct6/Dieta.rb
@@ -1,13 +1,41 @@
 class Dieta
-
-
-    def initialize()
-     
+  
+    attr_reader :nombre
+    def initialize(nombre,porc)
+        @nombre = nombre
+        @porcent = porc
     end
     
     def m_nombre()
        "#{@nombre} (#{@porcent}%)"
     end
+    
+    def m_raciones(comida,prop_aprox,prop_exact)
+        @food = Raciones.new(comida,prop_aprox,prop_exact)
+        @food.comidayprop()
+        @food.vct()
+    end
+end
+
+class Raciones
+    def initialize(comida,prop_aprox,prop_exact)
+        @comida = comida
+        @prop_aprox = prop_aprox
+        @prop_exact = prop_exact
+    end
+    
+    def comidayprop()
+		aux=""
+		size=@comida.length
+		for i in (0..size-1)
+			aux<<"- #{@comida[i]}#{@prop_aprox[i]}#{@prop_exact[i]}"
+			
+			i+=1
+			
+			aux+="\n"
+		end
+		"#{aux}"
+	end
 end
 
 
