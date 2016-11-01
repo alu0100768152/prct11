5189569658bd8ceaf1cb1a29c68b7d178da5b703
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-11-01 14:00:38 +0000

    Creando clase con nombre y porcentaje

--- lib/prct6/Dieta.rb
+++ lib/prct6/Dieta.rb
@@ -1,12 +1,14 @@
-
 class Dieta
   
-
-    def initialize()
-     
+    attr_reader :nombre
+    def initialize(nombre,porc)
+        @nombre = nombre
+        @porcent = porc
     end
-
     
+    def m_nombre()
+       "#{@nombre} (#{@porcent}%)"
+    end
 end
 
 
