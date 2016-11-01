173c499474df8df5a3008a353348f4e45d572fed
Cristian Ángel Díaz <alu0100891843@ull.edu.es>
2016-11-01 19:57:23 +0000

    Fallo por falta de definición de función comidayprop

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -5,7 +5,11 @@ require "./lib/prct6/Dieta"
     
     @nombre = 'ALMUERZO'
     @porcent= '30-35'
+    @comida = ["Macarrones con salsa de tomate y queso parmesano, ","Escalope de ternera, ","Ensalada básica con zanahoria rallada, ","Mandarina, ","Pan de trigo integral, "]
+    @prop_aprox = ["1 1/2 cucharón, ","1 bistec mediano, ", "", "1 grande, ", "1 rodaja, "]
+    @prop_exact = ["200 g","100 g","guarnición de 120 g", "180 g", "20 g"]
     objeto = Dieta.new(@nombre, @porcent)
+    comida = Raciones.new(@comida,@prop_aprox,@prop_exact)
     
     context "Prueba de las partes fundamentales de la clase Dieta:" do
     
@@ -18,8 +22,12 @@ require "./lib/prct6/Dieta"
         expect(objeto).not_to be_nil
       end
       
-      it "nombre de la dieta" do
+      it "Nombre de la dieta" do
         expect(objeto.m_nombre()).to eq("ALMUERZO (30-35%)")
+      end
+      
+      it "Raciones de comida" do
+        expect(comida.comidayprop()).to eq("- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición de 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g\n")
       end
     end
   end
