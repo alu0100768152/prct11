63bfffce10fafd3c12126e1f6a2e3e156fa4186c
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-11-08 22:06:18 +0000

    Failures:
      1) Linked Prueba de las partes fundamentales de la clase Linked: Pueba de los metodos secundarios de la clase Linked: Extrael primer elemento
         Failure/Error: lista.ex_1er_elemento()
    
         NoMethodError:
           undefined method `ex_1er_elemento' for #<Linked:0x00000001be4f78>
         # ./spec/prct6_spec.rb:59:in `block (4 levels) in <top (required)>'

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -40,15 +40,27 @@ require "spec_helper"
       it "-> Debe existir un objeto y estar inicializado." do
         expect(lista).not_to be_nil
       end
+      
+      it "-> El head de la lista no puede estar a nil si hay elementos en la lista " do
+        expect(lista.head.value).not_to be_nil
+      end
             
       it "-> En la posición[0][0] del vector tiene que estar DESAYUNO " do
         lista.head.value[0][0].should eq("DESAYUNO")
       end
       
+      
       it "Menú dietético DESAYUNO" do
         expect(lista.to_s()).to eq("\nDESAYUNO (15%)\n\n- Leche desnatada, 1 vaso, 200 ml\n- Cacao instantaneo, 1 c/sopera, 10 g\n- Cereales de desayuno en hojuelas, 1 bol peque~no, 40 g\n- Almendras laminadas (10 unidades), 2 c/soperas, 10 g\n\nV.C.T. |%    288,0 kcal | 17% - 21% - 62% \n\n\nMEDIA MANANA (10%)\n\n- Cerezas, 10-12 unidades medianas, 120 g\n- Galletas bífidus con sésamo, 4 unidades, 40 g\n\nV.C.T. |%    255,5 kcal | 7% - 24% - 69% \n\n\nALMUERZO (30-35%)\n\n- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición de 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g\n\nV.C.T. |%    785.9 kcal | 19% - 34% - 47% \n\n\nMERIENDA (15%)\n\n- Galletas de leche con chocolate y yogur, 4 unidades, 46 g\n- Flan de vainilla sin huevo, 1 unidad, 110 g\n\nV.C.T. |%    313,6 kcal | 10% - 30% - 60% \n\n\nCENA (25-30%)\n\n- Crema de bubango, 2 cucharones, 200 g\n- Tortilla campesina con espinacas, 1 cuña grande, 150 g\n- Tomate en dados con atún, 5 a 6 c/soperas, 150 g\n- Piña natural o en su jugo picada, 5 c/soperas, 120 g\n- Pan de trigo integral, 1 rodaja, 20 g\n\nV.C.T. |%    561,6 kcal | 19% - 40% - 41% \n\n")
       end
       
+    context "Pueba de los metodos secundarios de la clase Linked:" do  
+      it "Extrael primer elemento" do
+        lista.ex_1er_elemento()
+        expect(lista.to_s()).to eq("\nALMUERZO (30-35%)\n\n- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición de 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g\n\nV.C.T. |%    785.9 kcal | 19% - 34% - 47% \n\n")
+      end
+      
+    end
       
     end
   end
