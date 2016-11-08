8774fd2d51edde4297eafc83a4a1248aa386f093
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-11-08 20:30:30 +0000

    Creando los dos primeros vectores

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -3,10 +3,13 @@ require "spec_helper"
 
   describe Linked do
     
+    a=["DESAYUNO", "15"]
+    b=[["Leche desnatada, ","Cacao instantaneo, ","Cereales de desayuno en hojuelas, ","Almendras laminadas (10 unidades), "],["1 vaso, ","1 c/sopera, ", "1 bol peque~no, ", "2 c/soperas, "],["200 ml","10 g","40 g", "10 g"],"288,0",["17","21","62"] ]
 
     lista = Linked.new()
     array1= [a,b]
     lista.add(array1)
+
     
     context "Prueba de las partes fundamentales de la clase Linked:" do
     
