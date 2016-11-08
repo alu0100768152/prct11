0c6eaec98a292e0fc7fb15db35aa1e5b68772a5b
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-11-08 20:59:54 +0000

    Comprobando que funcionan dos elementos de la lista

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -6,9 +6,15 @@ require "spec_helper"
     a=["DESAYUNO", "15"]
     b=[["Leche desnatada, ","Cacao instantaneo, ","Cereales de desayuno en hojuelas, ","Almendras laminadas (10 unidades), "],["1 vaso, ","1 c/sopera, ", "1 bol peque~no, ", "2 c/soperas, "],["200 ml","10 g","40 g", "10 g"],"288,0",["17","21","62"] ]
 
+    
+    e=["ALMUERZO", "30-35"]
+    f=[["Macarrones con salsa de tomate y queso parmesano, ","Escalope de ternera, ","Ensalada básica con zanahoria rallada, ","Mandarina, ","Pan de trigo integral, "],["1 1/2 cucharón, ","1 bistec mediano, ", "", "1 grande, ", "1 rodaja, "],["200 g","100 g","guarnición de 120 g", "180 g", "20 g"],"785.9",["19","34","47"] ]
+
     lista = Linked.new()
     array1= [a,b]
+    array3= [e,f]
     lista.add(array1)
+    lista.add(array3)
 
     
     context "Prueba de las partes fundamentales de la clase Linked:" do
@@ -26,7 +32,7 @@ require "spec_helper"
       end
       
       it "Menú dietético DESAYUNO" do
-        expect(lista.to_s()).to eq("\nDESAYUNO (15%)\n\n- Leche desnatada, 1 vaso, 200 ml\n- Cacao instantaneo, 1 c/sopera, 10 g\n- Cereales de desayuno en hojuelas, 1 bol peque~no, 40 g\n- Almendras laminadas (10 unidades), 2 c/soperas, 10 g\n\nV.C.T. |%    288,0 kcal | 17% - 21% - 62% \n\n")
+        expect(lista.to_s()).to eq("\nDESAYUNO (15%)\n\n- Leche desnatada, 1 vaso, 200 ml\n- Cacao instantaneo, 1 c/sopera, 10 g\n- Cereales de desayuno en hojuelas, 1 bol peque~no, 40 g\n- Almendras laminadas (10 unidades), 2 c/soperas, 10 g\n\nV.C.T. |%    288,0 kcal | 17% - 21% - 62% \n\n\nALMUERZO (30-35%)\n\n- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición de 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g\n\nV.C.T. |%    785.9 kcal | 19% - 34% - 47% \n\n")
       end
       
       
