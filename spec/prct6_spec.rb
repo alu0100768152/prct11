90fca5ba8e80910bda549093659fb1410cc940ae
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-12-13 18:37:50 +0000

    Failures:
      1) Linked -> Dieta Lunes
         Failure/Error: @nomb=@aux.value[0][0]
    
         NoMethodError:
           undefined method `[]' for #<MenuDSL:0x0000000150da88>
         # ./lib/prct6/linked.rb:39:in `to_s'
         # ./spec/prct6_spec.rb:142:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00752 seconds (files took 0.12719 seconds to load)
    12 examples, 1 failure

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -60,7 +60,7 @@ require "spec_helper"
       
       @lista_M = Linked.new()
       
-      desayuno_L = MenuDSL.new("Ejemplo") do
+      desayuno_L = MenuDSL.new("Dieta mediterranea") do
   
         title  "Desayuno"
         ingest  :max => 15
@@ -78,7 +78,7 @@ require "spec_helper"
       
       end  
       
-      almuerzo_L = MenuDSL.new("Ejemplo") do
+      almuerzo_L = MenuDSL.new("Dieta mediterranea") do
   
         title  "Almuerzo"
         ingest  :min => 30, :max => 35
@@ -97,7 +97,7 @@ require "spec_helper"
       
       end 
       
-      cena_L = MenuDSL.new("Ejemplo") do
+      cena_L = MenuDSL.new("Dieta mediterranea") do
   
         title  "Cena"
         ingest  :min => 15, :max => 20
@@ -137,6 +137,10 @@ require "spec_helper"
     it "-> El head de la lista_L no puede estar a nil si hay elementos en la lista " do
       expect(@lista_L.head.value).not_to be_nil
     end
+    
+    it "-> Dieta Lunes" do
+        expect(@lista_L.to_s).to eq("Dieta mediterranea\n=======\n\nDesayuno [15]\n- Leche desnatada,  1 vaso, 200g\n- Cacao instantaneo, 1 c/sopera, 10g\n- Medio bocadillo de jamón y queso, Mitad de barra, 200g\n- Almendras laminadas, (10 unidades) 2 c/soperas, 10g\nV.C.T. | %   288.0 kcal | 48% - 49% - 34%\nDieta mediterranea\n=======\n\nAlmuerzo [30, 35]\n- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200g\n- Escalope de ternera, 1 bistec mediano, 100g\n- Ensalada básica con zanahoria rallada, guarnición, 120g\n- Mandarina, 1 grande, 180g\n- Pan de trigo integral, 1 rodaja, 20g\nV.C.T. | %   785.9 kcal | 19% - 34% - 47%\nDieta mediterranea\n=======\n\nCena [15, 20]\n- Pizza 4 quesos, 1 pizza, 300g\n- Yogur de coco, 1 vasito, 125g\n- Mandarina, 1 grande, 180g\nV.C.T. | %   489.9 kcal | 15% - 29% - 63%\n")
+    end 
     
   end
   
