defc75767b39b10244779ab5af4180b987661f6e
Óscar Jesús Socas González <alu0100768152@ull.edu.es>
2016-12-13 18:44:15 +0000

    Arreglando to_s de la clase Linked

--- lib/prct6/linked.rb
+++ lib/prct6/linked.rb
@@ -29,53 +29,15 @@ class Linked
     
     def to_s
         
-        @aux=head
-        #@tam=@aux.value.length
+       @aux=head
         aux2=""
         
-        
         while @aux != nil do
+
+            aux2 += "#{@aux.value.to_s}"
             
-            @nomb=@aux.value[0][0]
-            @porc=@aux.value[0][1]
-            
-            @comida = @aux.value[1][0]
-            @prop_aprox = @aux.value[1][1]
-            @prop_exact = @aux.value[1][2]
-            @kcal = @aux.value[1][3]
-            @por = @aux.value[1][4]
-           
-            if (@aux.value[1][5]==nil)
-                
-                ob_diet=Dieta.new(@nomb,@porc,@comida,@prop_aprox,@prop_exact,@kcal,@por)
-                #ob_raci=Raciones.new(@comida,@prop_aprox,@prop_exact,@kcal,@por)
-                        
-                aux2+="\n#{ob_diet.m_nombre()}\n"
-                aux2+="#{ob_diet.comidayprop()}\n"
-                aux2+="#{ob_diet.vct()}\n"
-                 
-                
-                @aux=@aux.next
-            
-            else
-                
-                if (@aux.value[1][6]==nil)
-                    @tipo = @aux.value[1][5]
-                    ob_alimentos=Alimentos.new(@nomb,@porc,@comida,@prop_aprox,@prop_exact,@kcal,@por,@tipo)
-                    aux2+="#{ob_alimentos.to_s()}\n"
-                    
-                     @aux=@aux.next
-                else 
-                    @desde = @aux.value[1][5]
-                    @hasta = @aux.value[1][6]
-                    ob_edad=Edad.new(@nomb,@porc,@comida,@prop_aprox,@prop_exact,@kcal,@por,@desde,@hasta)
-                    aux2+="#{ob_edad.to_s()}\n"
-                    
-                     @aux=@aux.next
-                end
-            end    
+            @aux=@aux.next
         end
-        
         "#{aux2}"
       
     end 
